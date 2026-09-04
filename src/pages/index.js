239fe99857bb62012544be4ddaa6613efd23fa69
wngstnr-code <wangsitsada1234@gmail.com>
2026-09-04 08:58:59 +0700

    feat: ganti nama dan niu di index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Nama] | [NIU]
+            [Wangsit Nursyahada] | [545092]
           </h5>
         </div>
       </main>
